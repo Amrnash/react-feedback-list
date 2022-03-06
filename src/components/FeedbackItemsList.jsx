d7be0e98e79235fae3b5f9dad9a4ab14da2d5f30
Amr Nashaat <amrnashaat8@gmail.com>
2022-03-06 18:52:46 +0200

    Add animations for list items

--- src/components/FeedbackItemsList.jsx
+++ src/components/FeedbackItemsList.jsx
@@ -1,22 +1,25 @@
 import FeedbackItem from "./FeedbackItem";
 import { useContext } from "react";
 import feedbackContext from "../context/feedbackContext";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 function FeedbackItemsList() {
   const { feedbackItems, handleDelete, handleEditFeedback } =
     useContext(feedbackContext);
   return (
-    <>
-      {feedbackItems.map((item, idx) => (
-        <FeedbackItem
-          id={item.id}
-          text={item.text}
-          rating={item.rating}
-          key={item.id}
-          handleDelete={handleDelete}
-          handleEditFeedback={handleEditFeedback}
-        />
+    <TransitionGroup>
+      {feedbackItems.map((item) => (
+        <CSSTransition key={item.id} timeout={500} classNames="item">
+          <FeedbackItem
+            id={item.id}
+            text={item.text}
+            rating={item.rating}
+            key={item.id}
+            handleDelete={handleDelete}
+            handleEditFeedback={handleEditFeedback}
+          />
+        </CSSTransition>
       ))}
-    </>
+    </TransitionGroup>
   );
 }
 
